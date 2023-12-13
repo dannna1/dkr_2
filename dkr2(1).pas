3305ac43363907e57070f170917b439267eacb73
dannna1 <144677250+dannna1@users.noreply.github.com>
2023-12-13 16:51:47 +0300

    Add files via upload

---
+++ dkr2(1).pas
@@ -0,0 +1,33 @@
+﻿const
+  ARRAY_SIZE = 10;
+type
+  IntArray = array[1..ARRAY_SIZE] of integer;
+var
+  arr: IntArray;
+  searchElement: integer;
+  i: integer;
+  isFound: boolean;
+begin
+  for i:= 1 to ARRAY_SIZE do
+  begin
+    Writeln('Введите массив:');
+    Read(arr[i]);
+  end;
+  WriteLn;
+  Write('Введите элемент для поиска: ');
+  ReadLn(searchElement);
+  isFound := false;
+  for i := 1 to ARRAY_SIZE do
+  begin
+    if arr[i] = searchElement then
+    begin
+      isFound := true;
+      Break;
+    end;
+  end;
+  if isFound then
+    WriteLn('Элемент ', searchElement, ' найден в массиве.')
+  else
+    WriteLn('Элемент ', searchElement, ' не найден в массиве.');
+
+end.
---
+++ dkr2(2).pas
@@ -0,0 +1,22 @@
+﻿var
+  str: string;
+  i, l: integer;
+  P: boolean;
+begin
+  write('Введите строку: ');
+  readln(str);
+  l:= Length(str);
+  P:= true;
+  for i:= 1 to l div 2 do
+  begin
+    if (LowerCase(str[i]) <> LowerCase(str[l - i + 1])) then
+    begin
+      P:= false;
+      break;
+    end;
+  end;
+  if P then
+    writeln('Строка является палиндромом')
+  else
+    writeln('Строка не является палиндромом');
+end.
